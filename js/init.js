bd7815d96a02dadd207c81c2a8c4b8044f76686d
GarciaGacia <76066714+GarciaGacia@users.noreply.github.com>
2021-01-23 16:44:53 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,17 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase. */
+// @ts-ignore
+firebase.initializeApp({
+     apiKey: "AIzaSyBKLNLIGXhCxDAOGZO0woJeQJJQDT4oZX0",
+    authDomain: "gatalan-1de74.firebaseapp.com",
+    projectId: "gatalan-1de74",
+    storageBucket: "gatalan-1de74.appspot.com",
+    messagingSenderId: "62880048232",
+    appId: "1:62880048232:web:992d8745f129de5778c7dd"
+    });
+
+
+
+   
+  
+ 
